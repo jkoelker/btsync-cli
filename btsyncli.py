e72195a19bcf724a52164e29bba301cd122b2d77
Jason Kölker <jason@koelker.net>
2014-04-26 05:40:28 +0000

    Subcommand support
    Also adds support for command line flags for config, user, pass, host,
    and port

--- btsyncli.py
+++ btsyncli.py
@@ -2,18 +2,18 @@
 """ cli interface for btsync """
 
 import argparse
-import btsync
+import getpass
 import locale
 import os
 import sys
 
+import btsync
+import blessings
 
 try:
     import simplejson as json
 except ImportError:
     import json
-
-from blessings import Terminal
 
 
 def sync_dir_exists(client, path):
@@ -81,7 +81,7 @@ def pprint_sync_folders(sync_folders):
     status_string -- btsync.Client.sync_folders
     """
 
-    term = Terminal()
+    term = blessings.Terminal()
     width = term.width
     for folder in sync_folders:
         print(term.green(folder['name']))
@@ -95,6 +95,11 @@ def main():
     """ main """
     parser = argparse.ArgumentParser(
         description='A command line interface for btsync.')
+
+    commands = parser.add_subparsers(dest='command')
+
+    commands.add_parser('list')
+
     parser.add_argument('-l', '--list', action='store_true', dest='l',
                         help='list sync folders')
     parser.add_argument('-a', '--add', action='store', dest='folder',
@@ -104,32 +109,54 @@ def main():
 #                        help='dump json output for all sync folders')
 #    parser.add_argument('-i', '--import', action='store_true', dest='import',
 #                        help='add all folders in a json dump')
-#    parser.add_argument('-c', '--config', action='store_true', dest='config',
-#                        help='use values in you btsync config file')
-#    parser.add_argument('-H', '--host', action='store_true', dest='host',
-#                        help='the host of your btsync webservice')
-#    parser.add_argument('-u', '--user', action='store_true', dest='user',
-#                        help='your btsync username')
-#    parser.add_argument('-p', '--pass', action='store_true', dest='pass',
-#                        help='prompt for btsync password')
+    parser.add_argument('-c', '--config', action='store_true', dest='config',
+                        default='~/.sync/sync.conf',
+                        help='use values in you btsync config file')
+    parser.add_argument('-H', '--host', dest='host', default='127.0.0.1',
+                        help='the host of your btsync webservice')
+    parser.add_argument('-P', '--port', dest='port', default='8888',
+                        help='the port of your btsync webservice')
+    parser.add_argument('-u', '--user', dest='username',
+                        help='your btsync username')
+    parser.add_argument('-p', '--pass', action='store_true', dest='password',
+                        help='prompt for btsync password')
     parser.add_argument('-s', '--secret', action='store', default=None,
                         dest='secret',
                         help='a btsync secret')
 
     args = parser.parse_args()
-    config_file = '/var/lib/btsync/.sync/sync.conf'
-    with open(config_file) as config_file:
-        config = json.load(config_file)
 
-    sync_client = btsync.Client(
-        host=config['webui']['listen'].split(':')[0],
-        port=config['webui']['listen'].split(':')[1],
-        username=config['webui']['login'],
-        password=config['webui']['password'])
+    config_file = os.path.expanduser(args.config)
 
-    if args.l:
+    try:
+        with open(config_file) as config_file:
+            config = json.load(config_file)
+            host, port = config['webui']['listen'].split(':')
+            username = config['webui']['login']
+            password = config['webui']['password']
+
+    except IOError:
+        host = args.host
+        port = args.port
+        username = args.username or getpass.getuser()
+
+        if args.password:
+            password = getpass.getpass()
+
+        else:
+            password = None
+
+    if not password:
+        print 'Password not found, use either --config or --pass'
+        sys.exit(1)
+
+    sync_client = btsync.Client(host=host, port=port, username=username,
+                                password=password)
+
+    if args.command == 'list':
         pprint_sync_folders(sync_client.sync_folders)
-    if args.folder:
+
+    elif args.folder:
         sync_path = os.path.abspath(args.folder)
         add_sync_folder(sync_client, sync_path, args.secret)
 
